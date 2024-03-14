afc16b977008914bbc80d92eef206e3ef607fb83
nasirabbaskhan <akhtarabbas.islamicreaction@gmail.com>
2024-03-14 08:29:41 -0700

    added addToCart functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers(){
+        return[
+            {
+               source:"/api/:path*",
+               headers:[
+                { key:"Access-Control-allow-Credentials",value:"true" },
+                { key:"Access-Control-allow-Origin",value:"*"},
+                { key:"Access-Control-allow-Methods",value:"GET,POST,PUT,DELETE"},
+               ]
+            }
+        ]
+    },
     images:{
         remotePatterns:[
         {
